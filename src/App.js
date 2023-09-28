9ed2fac1f74b4ef9c2031bfedebabf483eace694
Saif <saif@Arifs-MBP-4.lan>
2023-09-28 14:11:19 -0400

    Extracting components

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+    // Tells react to find a variable called "(any var name)" inside our component,
+    // take whatever val it has and print it inside h1 tag with the var name inside brackets
+    return <h1>Bye there!</h1>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 // 1. Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2. Get a refernce to the div with ID root
 const el = document.getElementById('root');
 
 // Tel React to take control of that element
 const root = ReactDOM.createRoot(el);
-
-// 4. Create a component(component is a function that returns some JSX)
-function App() {
-    const name = 'Samantha';
-    const age = 23;
-    // Tells react to find a variable called "(any var name)" inside our component,
-    // take whatever val it has and print it inside h1 tag with the var name inside brackets
-    return (
-        <h1>
-            Hi, my name is {name} and my age is {age}
-        </h1>
-    );
-}
 
 // 5. Show the component on the screen
 root.render(<App />);
